bf64ebf9e4ea56a4dfe3c9fb3c8c730c84b47861
erick <ericklarsenn@gmail.com>
2021-06-01 23:39:22 +0700

    Add footer

--- src/components/layouts/MainLayout.jsx
+++ src/components/layouts/MainLayout.jsx
@@ -1,8 +1,14 @@
 import React from "react";
 import PropTypes from "prop-types";
+import Footer from "../organisms/Footer";
 
 const Layout = ({ children }) => {
-  return <div className="w-full bg-gradient-to-b from-dark-200  to-dark-500 overflow-x-hidden">{children}</div>;
+  return (
+    <div className="w-full bg-gradient-to-b from-dark-200  to-dark-500 overflow-x-hidden">
+      {children}
+      <Footer />
+    </div>
+  );
 };
 
 Layout.propTypes = {};
