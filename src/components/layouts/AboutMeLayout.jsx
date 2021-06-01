82a59ecfab089ff9d7ec4a278a3a1ff20238ee6e
erick <ericklarsenn@gmail.com>
2021-06-01 22:34:01 +0700

    Set initial about screen components

---
+++ src/components/layouts/AboutMeLayout.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const AboutMeLayout = ({ children }) => {
+  return <div className="w-full min-h-screen pb-10 relative">{children}</div>;
+};
+
+AboutMeLayout.propTypes = {};
+
+export default AboutMeLayout;
