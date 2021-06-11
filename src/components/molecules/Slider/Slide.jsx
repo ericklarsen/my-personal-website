44d536e6e22d73142643b1989b09959c5c9da31b
erick <ericklarsenn@gmail.com>
2021-06-12 00:30:30 +0700

    Slider molecules

---
+++ src/components/molecules/Slider/Slide.jsx
@@ -0,0 +1,69 @@
+import React, { useEffect, useState } from "react";
+import PropTypes from "prop-types";
+import SliderSlide from "./SliderSlide";
+import UseIsMobile from "../../../hooks/UseIsMobile";
+
+const Slide = ({ totalSlides, data, onShow }) => {
+  //   const [contentPerSlide, setContentPerSlide] = useState(4);
+  const isMobile = UseIsMobile();
+  let position = 0;
+  let contentPerSlide = 4;
+
+  useEffect(() => {
+    if (isMobile) {
+      contentPerSlide = 2;
+      //   setContentPerSlide(2);
+      console.log("tes");
+    }
+  }, [isMobile]);
+
+  console.log(contentPerSlide);
+
+  return [...Array(totalSlides).keys()].map((item, i) => {
+    let mainIdx = i + 1;
+    return (
+      <SliderSlide key={i} column={2}>
+        {data.map((item, j) => {
+          let subIdx = j + 1;
+
+          if (mainIdx === 1 && subIdx <= mainIdx * contentPerSlide) {
+            position = subIdx;
+            return (
+              <div
+                key={j}
+                className="relative w-full h-56 md:h-80 rounded-2xl cursor-pointer"
+                onClick={() => onShow(item.id)}
+              >
+                <div className="absolute top-0 left-0 w-full h-full bg-dark-500 opacity-50 rounded-2xl z-10 hover:bg-transparent transition-all"></div>
+                <img
+                  src={item.mainImage}
+                  alt="images"
+                  className="w-full h-full object-cover rounded-2xl"
+                />
+              </div>
+            );
+          } else if (mainIdx !== 1 && subIdx > position && subIdx <= mainIdx * contentPerSlide) {
+            position = subIdx;
+            return (
+              <div
+                key={j}
+                className="w-full bg-white opacity-10 h-56 md:h-80 rounded-2xl cursor-pointer"
+                onClick={() => onShow(item.id)}
+              >
+                <img
+                  src={item.mainImage}
+                  alt="images"
+                  className="w-full h-full object-cover rounded-2xl"
+                />
+              </div>
+            );
+          }
+        })}
+      </SliderSlide>
+    );
+  });
+};
+
+Slide.propTypes = {};
+
+export default Slide;
---
+++ src/components/molecules/Slider/SliderBubbleButton.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const SliderBubbleButton = ({ totalSlides, activeSlide, leftMove, rightMove, bubbleMove }) => {
+  return (
+    <div className="flex justify-center items-center flex-shrink-0">
+      <img
+        src="/img/leftArrow.svg"
+        alt="left arrow"
+        onClick={leftMove}
+        className={`${activeSlide === 1 && "opacity-20"} cursor-pointer`}
+      />
+      <div className="flex mx-8 ">
+        {[...Array(totalSlides).keys()].map((i) => (
+          <div
+            key={i}
+            onClick={() => bubbleMove(i)}
+            className={`w-3 h-3 rounded-full cursor-pointer bg-white ${
+              activeSlide === i + 1 ? "opacity-50" : "opacity-10"
+            } ${i !== totalSlides - 1 && "mr-2.5"}`}
+          ></div>
+        ))}
+      </div>
+      <img
+        src="/img/rightArrow.svg"
+        alt="right arrow"
+        onClick={rightMove}
+        className={`${activeSlide === totalSlides && "opacity-20"} cursor-pointer`}
+      />
+    </div>
+  );
+};
+
+SliderBubbleButton.propTypes = {};
+
+export default SliderBubbleButton;
