934c1272a87611c94289c626176ff43a6bde11a7
erick <ericklarsenn@gmail.com>
2021-06-12 00:29:43 +0700

    Set responsiveness

--- src/components/layouts/PortfolioLayout.jsx
+++ src/components/layouts/PortfolioLayout.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import PropTypes from "prop-types";
 
 const PortfolioLayout = ({ children }) => {
-  return <div className="w-full min-h-screen pb-10 relative">{children}</div>;
+  return <div className="w-full max-w-screen-2xl m-auto relative lg:min-h-desktop lg:px-10 lg:flex lg:items-center lg:flex-row-reverse lg:justify-between xl:px-35 ">{children}</div>;
 };
 
 PortfolioLayout.propTypes = {};
