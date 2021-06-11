fcb89bafb74f95b2b99f9c723c7681f4547a2bdf
erick <ericklarsenn@gmail.com>
2021-06-12 00:25:33 +0700

    New styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -18,18 +18,42 @@ module.exports = {
       fontFamily: {
         "mont-black": ["Montserrat-Black"],
         "mont-bold": ["Montserrat-Bold"],
+        "mont-extra-bold": ["Montserrat-ExtraBold"],
         "mont-medium": ["Montserrat-Medium"],
         "mont-regular": ["Montserrat-Regular"],
       },
       keyframes: {
         fade: {
-          '0%': { opacity: "0" },
-          "100%": { opacity: "1" }
-        }
+          "0%": { opacity: "0" },
+          "100%": { opacity: "1" },
+        },
+        bottomsheet: {
+          "0%": { transform: "translateY(200%)" },
+          "100%": { transform: "translateY(0)" },
+        },
       },
       animation: {
-        fade: "fade 350ms ease-in-out"
-      }
+        fade: "fade 350ms ease-in-out",
+        bottomsheet: "bottomsheet 0.55s cubic-bezier(0.85,0.01,0.25,1)",
+      },
+      width: {
+        "105": "26.25rem",
+      },
+      height: {
+        content: "fit-content",
+        desktop: "1024px",
+      },
+      maxHeight: {
+        desktop: "1024px",
+      },
+      minHeight: {
+        desktop: "1024px",
+        sliderDesktop: "740px",
+        sliderMobile: "472px",
+      },
+      padding: {
+        35: "8.75rem",
+      },
     },
   },
   variants: {
