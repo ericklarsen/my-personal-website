d4ca2a14e9ad47cc25122e04c647fa2489d62e5a
erick <ericklarsenn@gmail.com>
2021-06-12 23:17:44 +0700

    Test

--- src/helpers/swipe_helper.js
+++ src/helpers/swipe_helper.js
@@ -5,8 +5,54 @@ export const generateSlider = (totalSlides, data, isMobile, onShow) => {
   let position = 0;
   let contentPerSlide = 4;
 
+  console.log(isMobile);
   if (isMobile) {
     contentPerSlide = 2;
+    return [...Array(totalSlides).keys()].map((item, i) => {
+      let mainIdx = i + 1;
+      return (
+        <SliderSlide key={i} column={2}>
+          {data.map((item, j) => {
+            let subIdx = j + 1;
+
+            if (mainIdx === 1 && subIdx <= mainIdx * contentPerSlide) {
+              position = subIdx;
+              return (
+                <div
+                  key={j}
+                  className="relative w-full h-56 md:h-80 rounded-2xl cursor-pointer"
+                  onClick={() => onShow(item.id)}
+                >
+                  <div className="hidden absolute top-0 left-0 w-full h-full bg-dark-500 opacity-50 rounded-2xl z-10 hover:bg-transparent transition-all lg:flex"></div>
+                  {/* <div className="absolute top-0 left-0 w-full h-full bg-dark-500 opacity-50 rounded-2xl z-10 hover:bg-transparent transition-all"></div> */}
+                  <img
+                    src={item.mainImage}
+                    alt="images"
+                    className="w-full h-full object-cover rounded-2xl"
+                  />
+                </div>
+              );
+            } else if (mainIdx !== 1 && subIdx > position && subIdx <= mainIdx * contentPerSlide) {
+              position = subIdx;
+              return (
+                <div
+                  key={j}
+                  className="relative w-full h-56 md:h-80 rounded-2xl cursor-pointer"
+                  onClick={() => onShow(item.id)}
+                >
+                  <div className="hidden absolute top-0 left-0 w-full h-full bg-dark-500 opacity-50 rounded-2xl z-10 hover:bg-transparent transition-all lg:flex"></div>
+                  <img
+                    src={item.mainImage}
+                    alt="images"
+                    className="w-full h-full object-cover rounded-2xl"
+                  />
+                </div>
+              );
+            }
+          })}
+        </SliderSlide>
+      );
+    });
   }
 
   return [...Array(totalSlides).keys()].map((item, i) => {
@@ -24,7 +70,7 @@ export const generateSlider = (totalSlides, data, isMobile, onShow) => {
                 className="relative w-full h-56 md:h-80 rounded-2xl cursor-pointer"
                 onClick={() => onShow(item.id)}
               >
-                <div className="absolute top-0 left-0 w-full h-full bg-dark-500 opacity-50 rounded-2xl z-10 hover:bg-transparent transition-all"></div>
+                <div className="hidden absolute top-0 left-0 w-full h-full bg-dark-500 opacity-50 rounded-2xl z-10 hover:bg-transparent transition-all lg:flex"></div>
                 <img
                   src={item.mainImage}
                   alt="images"
@@ -37,9 +83,10 @@ export const generateSlider = (totalSlides, data, isMobile, onShow) => {
             return (
               <div
                 key={j}
-                className="w-full bg-white opacity-10 h-56 md:h-80 rounded-2xl cursor-pointer"
+                className="relative w-full h-56 md:h-80 rounded-2xl cursor-pointer"
                 onClick={() => onShow(item.id)}
               >
+                <div className="hidden absolute top-0 left-0 w-full h-full bg-dark-500 opacity-50 rounded-2xl z-10 hover:bg-transparent transition-all lg:flex"></div>
                 <img
                   src={item.mainImage}
                   alt="images"
@@ -52,4 +99,6 @@ export const generateSlider = (totalSlides, data, isMobile, onShow) => {
       </SliderSlide>
     );
   });
+
+  console.log(isMobile);
 };
