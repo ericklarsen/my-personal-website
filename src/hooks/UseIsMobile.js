bf05113c7721b47d850185e02fd1b7587bce6715
erick <ericklarsenn@gmail.com>
2021-06-13 00:10:34 +0700

    Test

--- src/hooks/UseIsMobile.js
+++ src/hooks/UseIsMobile.js
@@ -6,11 +6,11 @@ import { useMediaQuery } from "react-responsive";
 const { laptop } = breakPoint;
 
 const UseIsMobile = () => {
-  const isMobile = useMediaQuery({ maxDeviceWidth: laptop - 1 });
-  const [mobile, setMobile] = useState(true);
+  const isMobile = useMediaQuery({ minDeviceWidth: laptop - 1 });
+  const [mobile, setMobile] = useState(false);
 
   useEffect(() => {
-    setMobile(isMobile);
+    setMobile(!isMobile);
   }, [isMobile]);
 
   return mobile;
