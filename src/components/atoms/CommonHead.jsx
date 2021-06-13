5203dcdb2fe01bda1a2932773aff7d31eca0af2d
erick <ericklarsenn@gmail.com>
2021-06-13 22:12:12 +0700

    Update favicon

--- src/components/atoms/CommonHead.jsx
+++ src/components/atoms/CommonHead.jsx
@@ -9,7 +9,7 @@ function CommonHead(props) {
       <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
       <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
       <meta name="theme-color" content="#434343" />
-      <link rel="icon" href="/favicon.svg" />
+      <link rel="icon" href="/favicon.ico" />
       {/* <link rel="manifest" href="/manifest.json" /> */}
       {/* <link rel="apple-touch-icon" href="/apple-touch-icon-180x180.png" /> */}
       {props.children}
