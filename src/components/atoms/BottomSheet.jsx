508fa17c90d3682227f4201f2d8eafd4760dad49
erick <ericklarsenn@gmail.com>
2021-06-13 18:58:07 +0700

    Change zindex value

--- src/components/atoms/BottomSheet.jsx
+++ src/components/atoms/BottomSheet.jsx
@@ -6,18 +6,23 @@ const BottomSheet = ({ children, show, onShow }) => {
     return (
       <>
         <div
-          className="md:hidden w-screen h-screen fixed z-10 left-0 top-0 animate-fade"
+          className="md:hidden w-screen h-screen fixed z-50 left-0 top-0 animate-fade"
           style={{ background: "rgba(41, 41, 41, 0.85)" }}
         ></div>
         <div
-          className="md:hidden w-screen fixed h-content p-12 pt-16 bg-white bottom-0 left-0 z-20 rounded-t-3xl bg-gradient-to-b from-dark-200  to-dark-500 animate-bottomsheet"
+          className="md:hidden w-screen fixed h-content p-12 pt-16 bg-white bottom-0 left-0 z-100 rounded-t-3xl bg-gradient-to-b from-dark-200  to-dark-500 animate-bottomsheet"
           style={{ borderTop: `1px solid rgba(255, 255, 255, 0.17)` }}
         >
           <div
             className="absolute h-1 w-28 bg-white opacity-10 m-auto top-4 left-1/2"
             style={{ marginLeft: "-55px" }}
           ></div>
-          <img src="/img/close.svg" alt="close" className="absolute right-5 top-4" onClick={onShow} />
+          <img
+            src="/img/close.svg"
+            alt="close"
+            className="absolute right-5 top-4"
+            onClick={onShow}
+          />
           {children}
         </div>
       </>
@@ -27,6 +32,10 @@ const BottomSheet = ({ children, show, onShow }) => {
   return <div></div>;
 };
 
-BottomSheet.propTypes = {};
+BottomSheet.propTypes = {
+  children: PropTypes.node,
+  show: PropTypes.bool,
+  onShow: PropTypes.func,
+};
 
 export default BottomSheet;
