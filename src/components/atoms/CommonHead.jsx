6e8dab1aba438b41dac93ab70bc17b5f149ef51c
erick <ericklarsenn@gmail.com>
2021-06-18 23:41:55 +0700

    Enable pwa manifest

--- src/components/atoms/CommonHead.jsx
+++ src/components/atoms/CommonHead.jsx
@@ -8,10 +8,10 @@ function CommonHead(props) {
       <meta charSet="utf-8" />
       <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
       <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
-      <meta name="theme-color" content="#434343" />
+      <meta name="theme-color" content="#353535" />
       <link rel="icon" href="/favicon.svg" />
-      {/* <link rel="manifest" href="/manifest.json" /> */}
-      {/* <link rel="apple-touch-icon" href="/apple-touch-icon-180x180.png" /> */}
+      <link rel="manifest" href="/manifest.json" />
+      <link rel="apple-touch-icon" href="/apple-touch-icon-180.png" />
       {props.children}
     </Head>
   );
